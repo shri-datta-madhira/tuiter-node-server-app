f5b8f01bcc92c9f5fa880058701c9a5a4ba36f23
Shri Datta Madhira <madhira.s@northeastern.edu>
2022-11-14 12:29:24 -0500

    Assignment-8: Part-6 Done.

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -4,8 +4,7 @@ let tuits = posts;
 const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 0;
-    newTuit.liked = false;
+    console.log(newTuit)
     tuits.push(newTuit);
     res.json(newTuit);
 }
