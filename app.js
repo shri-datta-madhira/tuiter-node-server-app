7ef003a52f1b34b17113bbcca35be000e9e4a0eb
Shri Datta Madhira <madhira.s@northeastern.edu>
2022-11-27 10:05:06 -0500

    a9 small changes.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://0.0.0.0:27017/tuiter'
+console.log(process.env.DB_CONNECTION_STRING)
 console.log(CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING);
 const app = express()
